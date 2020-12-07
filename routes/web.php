a2978eba341bb45a597bd59c37cd97d895882098
Thet Minn Htun <thetminnhtun92@gmail.com>
2020-12-07 23:15:39 +0630

    install laravel ui

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,16 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
+    App\Models\User::where('id', '>', 1)
+    ->where(function ($query)
+    {
+        $query->where('name', 'like', '%min%')
+        ->where('updated_at', '>', now());
+    })
+    ->orderBy('id')->paginate();
     return view('welcome');
 });
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
